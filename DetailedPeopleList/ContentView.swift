8898cd652879e7a9ec7296988da42672ec5edaf0
DariaZvezdochkina <d.zvezdochkina@mail.ru>
2022-02-03 12:27:09 +0200

    Add DetailedDataView.swift

--- DetailedPeopleList/ContentView.swift
+++ DetailedPeopleList/ContentView.swift
@@ -8,31 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-  @ObservedObject private var viewModel: ContentViewModel
-  
-  init(viewModel: ContentViewModel = .init()) {
-    self.viewModel = viewModel
-  }
-  
-  var body: some View {
-    List{
-      ForEach(viewModel.results, id: \.url) { item in
-        VStack {
-          Text(item.name)
+    
+    @ObservedObject private var viewModel: ContentViewModel
+    
+    init(viewModel: ContentViewModel = .init()) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        NavigationView {
+            List{
+                ForEach(viewModel.results, id: \.url) { item in
+                    NavigationLink(destination: DetailedDataView(person: item)) {
+                        VStack {
+                            Text(item.name)
+                        }
+                    }
+                    
+                }
+            }
+            .task {
+                await viewModel.fetchData()
+            }
         }
-      }
     }
-    .task {
-      await viewModel.fetchData()
-    }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
 
 
--- DetailedPeopleList/DetailedDataView.swift
+++ DetailedPeopleList/DetailedDataView.swift
@@ -7,43 +7,66 @@
 import SwiftUI
 
 struct DetailedDataView: View {
-  
-  private var person: Person
-  
-  init(person: Person) {
-    self.person = person
-  }
-  
-  var body: some View {
-    HStack{
-      Text("dbjwkebfj")
+    
+    private var person: Person
+    
+    init(person: Person) {
+        self.person = person
     }
-  }
-  
+    
+    var body: some View {
+        VStack{
+            List{
+                HStack{
+                    Text("Name")
+                    Spacer()
+                    Text(person.name)
+                }
+                HStack{
+                    Text("Height")
+                    Spacer()
+                    Text(person.height)
+                }
+                HStack{
+                    Text("Hair Color")
+                    Spacer()
+                    Text(person.hairColor)
+                }
+                HStack{
+                    Text("Skin Color")
+                    Spacer()
+                    Text(person.skinColor)
+                }
+            }
+            
+        }
+        
+    }
+    
 }
 
 struct DetailedDataView_Previews: PreviewProvider {
-  static var previews: some View {
-    DetailedDataView(
-      person: Person(
-        name: "Luke",
-        height: "n/a",
-        mass: "n/a",
-        hairColor: "n/a",
-        skinColor: "n/a",
-        eyeColor: "n/a",
-        birthYear: "n/a",
-        gender: .male,
-        homeworld: "n/a",
-        films: [],
-        species: [],
-        vehicles: [],
-        starships: [],
-        created: "n/a",
-        edited: "n/a",
-        url: "n/a"
-      )
-    )
-  }
+    static var previews: some View {
+        DetailedDataView(
+            person: Person(
+                name: "Luke",
+                height: "n/a",
+                mass: "n/a",
+                hairColor: "n/a",
+                skinColor: "n/a",
+                eyeColor: "n/a",
+                birthYear: "n/a",
+                gender: .male,
+                homeworld: "n/a",
+                films: [],
+                species: [],
+                vehicles: [],
+                starships: [],
+                created: "n/a",
+                edited: "n/a",
+                url: "n/a"
+            )
+        )
+    }
 }
 
